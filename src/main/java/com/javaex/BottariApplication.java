bcf1c5b903ef2e8d6bce5b0a402f880a2fbfa7d1
ahah2025 <adghty61@naver.com>
2025-08-14 17:54:29 +0900

    보따리-스프링으로 처음 만든것

---
+++ src/main/java/com/javaex/BottariApplication.java
@@ -0,0 +1,13 @@
+package com.javaex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BottariApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BottariApplication.class, args);
+	}
+
+}
